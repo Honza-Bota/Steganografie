470067691aec54f22658b5b4ba70e939e47bfbd6
Boťa Jan <botajan@email.cz>
2021-03-09 22:00:30 +0100

    Přidejte soubory projektu.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stega
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command, text, soubor;
+            bool correctInput;
+
+
+            Console.ReadLine();
+        }
+    }
+}
