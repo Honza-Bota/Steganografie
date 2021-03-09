5c942e0b4f381b2383485a11b85267eba3426c43
Boťa Jan <botajan@email.cz>
2021-03-09 22:58:30 +0100

    Základní architektura.

--- Program.cs
+++ Program.cs
@@ -10,11 +10,67 @@ namespace Stega
     {
         static void Main(string[] args)
         {
+            //zadané hodnoty
             string command, text, soubor;
             bool correctInput;
 
+            //rozdělení hodnot z pole
+            correctInput = SplitInput(args, out command, out text, out soubor);
 
-            Console.ReadLine();
+            //rozhodnutí o volání metody
+            if (!correctInput) Console.WriteLine("Nelze spustit program bez vstupních kritérií.");
+            if (command == "--hide" && text != "" && soubor.Length > 0 && soubor.Contains(".png")) Console.WriteLine(Hide(text, soubor));
+            else if (command == "--show" && text == "" && soubor.Length > 0 && soubor.Contains(".png")) Console.WriteLine(Show(soubor));
+            else if (command == "--help") Console.WriteLine(Help());
+            else Console.WriteLine("Neplatný vstup!");
+        }
+
+        private static string Hide(string text, string soubor)
+        {
+            return "Úspěšně zašifrováno.";
+        }
+
+        private static string Show(string soubor)
+        {
+            return "Úspěšně dešifrováno.";
+        }
+
+        private static string Help()
+        {
+            return $@"Nápověda:
+~ šifrování do obrázku:  --hide ""Text"" ""obrázek.png""
+~ dešifrování z obrázku: --show ""obrázek.png""
+~ nápověda: --help";
+        }
+
+        private static bool SplitInput(string[] data, out string command, out string text, out string soubor)
+        {
+            //ohlídání prázdného vstupu
+            if (data.Length != 0)
+            {
+                //přiřazení zadaného příkazu
+                command = data[0];
+
+                //pokud je zvolena možnost hide, načte text
+                text = "";
+                if (data.Length > 2 && command == "--hide")
+                {
+                    for (int i = 1; i < data.Length - 1; i++)
+                    {
+                        text += data[i];
+                    } 
+                }
+
+                //přiřazení názvu souboru
+                soubor = data[data.Length - 1];
+
+                //vrácení inforamce o korektním vstupu
+                return true;
+            }
+
+            //špatný/prázdný command >> vrátí chybu
+            command = text = soubor = "";
+            return false;
         }
     }
 }
